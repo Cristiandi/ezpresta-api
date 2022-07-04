ecea429ec7aa784584ccec2ac18b262cb85c3488
Cristian David Ippolito <cristiandavidippolito@gmail.com>
2022-07-04 00:02:37 +0000

    feat: adding logging

--- src/modules/epayco-transaction/epayco-transaction.service.ts
+++ src/modules/epayco-transaction/epayco-transaction.service.ts
@@ -73,7 +73,7 @@ export class EpaycoTransactionService extends BaseService<EpaycoTransaction> {
   }
 
   public async initConfirmation(input: any) {
-    Logger.log('initConfirmation', EpaycoTransactionService.name);
+    Logger.log(`initConfirmation ${input}`, EpaycoTransactionService.name);
 
     if (!input.x_extra1) {
       throw new BadRequestException('x_extra1 is required');
