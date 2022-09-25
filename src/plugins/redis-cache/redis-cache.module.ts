8a6f415a9dfba03a2f20b383db69856c9705dc2b
Cristian David Ippolito <cristiandavidippolito@gmail.com>
2022-09-25 00:46:53 +0000

    feat: checking when making a new connection in redis

--- src/plugins/redis-cache/redis-cache.module.ts
+++ src/plugins/redis-cache/redis-cache.module.ts
@@ -2,17 +2,22 @@ import * as redisStore from 'cache-manager-redis-store';
 
 import { Module, CacheModule } from '@nestjs/common';
 import { RedisCacheService } from './redis-cache.service';
+import { Logger } from '@nestjs/common/services';
 
 @Module({
   imports: [
     CacheModule.registerAsync({
-      useFactory: () => ({
-        store: redisStore,
-        host: process.env.REDIS_HOST,
-        port: +process.env.REDIS_PORT,
-        password: process.env.REDIS_PASSWORD,
-        name: process.env.REDIS_CLIENT_NAME,
-      }),
+      useFactory: () => {
+        Logger.log('Connecting to Redis Cache', 'RedisCacheModule');
+
+        return {
+          store: redisStore,
+          host: process.env.REDIS_HOST,
+          port: +process.env.REDIS_PORT,
+          password: process.env.REDIS_PASSWORD,
+          name: process.env.REDIS_CLIENT_NAME,
+        };
+      },
     }),
   ],
   providers: [RedisCacheService],
