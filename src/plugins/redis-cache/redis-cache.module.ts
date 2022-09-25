0f26148b8b0c73486da2558e61f8b0a0e5a934c8
Cristian David Ippolito <cristiandavidippolito@gmail.com>
2022-09-25 00:56:52 +0000

    feat: no need to check when connecting with redis

--- src/plugins/redis-cache/redis-cache.module.ts
+++ src/plugins/redis-cache/redis-cache.module.ts
@@ -2,14 +2,11 @@ import * as redisStore from 'cache-manager-redis-store';
 
 import { Module, CacheModule } from '@nestjs/common';
 import { RedisCacheService } from './redis-cache.service';
-import { Logger } from '@nestjs/common/services';
 
 @Module({
   imports: [
     CacheModule.registerAsync({
       useFactory: () => {
-        Logger.log('Connecting to Redis Cache', 'RedisCacheModule');
-
         return {
           store: redisStore,
           host: process.env.REDIS_HOST,
